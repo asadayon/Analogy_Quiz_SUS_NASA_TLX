e8e4f99c7580a60e88d1b0b13d6ef8a8a50d370e
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2025-08-06 07:46:51 -0400

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1053,10 +1053,10 @@ def load_questions(who):
 
 def run_quiz():
     questions = st.session_state.shuffled_questions
-
-    st.write(f"User: {st.session_state["username"]}")
-    st.write(f"Scenario: {st.session_state["selected_user"]}")
-    st.title("Multiple Choice Quiz")
+    st.title("Quiz")
+    st.write(f"**User**: {st.session_state["username"]}")
+    st.write(f"**Scenario**: {st.session_state["selected_user"]}")
+    
 
 
     user_answers = []
@@ -1135,6 +1135,7 @@ if st.session_state.page == "home":
 elif st.session_state.page == "quiz":
     run_quiz()
     
+
 
 
 
